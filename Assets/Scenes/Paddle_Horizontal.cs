d5183301c4623bc10492139ddbec720ac35c2cce
samueldavi99 <samuel_davi_2009@hotmail.com>
2019-04-17 18:54:56 -0300

    Initial commit

---
+++ Assets/Scenes/Paddle_Horizontal.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle_Horizontal : MonoBehaviour
+{
+    // how fast the paddle can move
+    public float MoveSpeed = 10f;
+
+    // how far up and down the paddle can move
+    public float MoveRange = 10f;
+
+    // whether this paddle can accept player input
+    public bool AcceptsInput = true;
+
+    void Update()
+    {
+        // does not accept input, abort
+        if (!AcceptsInput)
+            return;
+
+        //get user input
+        float input = Input.GetAxis("Horizontal");
+
+        // move paddle
+        Vector3 pos = transform.position;
+        pos.x += input * MoveSpeed * Time.deltaTime;
+
+        // clamp paddle position
+        pos.x = Mathf.Clamp(pos.x, -MoveRange, MoveRange);
+
+        // set position
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scenes/ScoreKeeper.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scorekeeper : MonoBehaviour
+{
+    // the maximum score a player can reach
+    public int ScoreLimit = 10;
+
+    // Player 1's score
+    private int p1Score = 0;
+
+    // Player 2's score
+    private int p2Score = 0;
+
+    // give the appropriate player a point
+    public void AddScore(int player)
+    {
+        // player 1
+        if (player == 1)
+        {
+            p1Score++;
+        }
+        // player 2
+        else if (player == 2)
+        {
+            p2Score++;
+        }
+
+        // check if either player reached the score limit
+        if (p1Score >= ScoreLimit || p2Score >= ScoreLimit)
+        {
+            // player 1 has a better score than player 2
+            if (p1Score > p2Score)
+                Debug.Log("Player 1 wins");
+            // player 2 has a better score than player 1
+            if (p2Score > p1Score)
+                Debug.Log("Player 2 wins");
+            // both players have the same score - tie
+            else
+                Debug.Log("Players are tied");
+
+            // reset scores and start over
+            p1Score = 0;
+            p2Score = 0;
+        }
+    }
+}
